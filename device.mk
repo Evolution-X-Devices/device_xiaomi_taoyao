e3f2d51d41672fc3e743b8b4b571edc9078f7b01
a <ammarbahtiarasli@gmail.com>
2026-08-14 05:42:10 +0000

    Revert "taoyao: Migrate to AIDL camera provider"
    This reverts commit f0d83cccb73d7cdd0b7dfdaad4d8c940f90e362a.

--- device.mk
+++ device.mk
@@ -127,7 +127,8 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     libutilscallstack.vendor \
-    android.hardware.camera.provider-service.lineage \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
     libcamera_metadata.vendor \
     android.hardware.graphics.common-V4-ndk \
     vendor.qti.hardware.camera.device@1.0.vendor \
